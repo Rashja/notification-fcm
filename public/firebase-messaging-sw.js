174e1d703eceeb8e307f9f2dda37b3dc112c1ed8
Ramtin <ramtinsharif@gmail.com>
2020-04-24 14:54:30 +0430

    clear firebase console info

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,13 +5,6 @@ importScripts(
 
 firebase.initializeApp({
   // Project Settings => Add Firebase to your web app
-  apiKey: "AIzaSyAl-PPKymCbC2seTUWXBaYHAAN3Hw0I8bg",
-  authDomain: "test-8699c.firebaseapp.com",
-  databaseURL: "https://test-8699c.firebaseio.com",
-  projectId: "test-8699c",
-  storageBucket: "test-8699c.appspot.com",
-  messagingSenderId: "484864170409",
-  appId: "1:484864170409:web:a998a5e9a58c9377574cb8",
 });
 const messaging = firebase.messaging();
 
--- src/push-notification.js
+++ src/push-notification.js
@@ -2,14 +2,7 @@ import firebase from "firebase";
 
 export const initializeFirebase = () => {
   firebase.initializeApp({
-    apiKey: "AIzaSyDMDZ0zS6oUNQpkv5ec8jtLPpMSEbkmDiI",
-    authDomain: "notif-test-b2733.firebaseapp.com",
-    databaseURL: "https://notif-test-b2733.firebaseio.com",
-    projectId: "notif-test-b2733",
-    storageBucket: "notif-test-b2733.appspot.com",
-    messagingSenderId: "325632137137",
-    appId: "1:325632137137:web:5548d4b54cae8760c64f90",
-    measurementId: "G-2KVK7LG6MQ",
+    // fire base console configue
   });
 };
 
